5d59a385e4fff891240818c01034979ee44e5d5e
Naitian Zhou <naitian@naitian.org>
2024-11-18 23:26:30 -0800

    Fix build

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from "mdsvex";
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { sveltePreprocess } from 'svelte-preprocess';
 
 
@@ -9,7 +9,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+		})
 	},
 
     preprocess: [sveltePreprocess(), mdsvex()],
